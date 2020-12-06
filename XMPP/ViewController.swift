7b40db62fa7a30f0238c744b75b7bfab64180d3c
loghozinsky <loghozinsky@gmail.com>
2020-12-06 13:59:39 +0200

    Initial Commit

---
+++ XMPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XMPP
+//
+//  Created by Aleksander Loghozinsky on 06.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
